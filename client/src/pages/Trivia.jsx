5015a9f62bfe21ac350fcbbfd379f3b1fd6ac17b
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-04-10 18:36:20 -0400

    updated images path

--- client/src/pages/Trivia.jsx
+++ client/src/pages/Trivia.jsx
@@ -23,140 +23,140 @@ const Trivia = () => {
         {
             id: 1,
             question: "What is the name of Arnold's best friend in 'Hey Arnold'?",
-            image: '../../public/images/gerald.jpg',
+            image: '../../images/gerald.jpg',
             options: ["Gerald", "Sid", "Stinky", "Harold"],
             answer: "Gerald"
         },
         {
             id: 2,
             question: "Who is the fearless dog protecting his owners from various monsters and villains?",
-            image: '../../public/images/courage.jpg',
+            image: '../../images/courage.jpg',
             options: ["Katz", "Muriel", "Eustace", "Courage the Cowardly Dog"],
             answer: "Courage the Cowardly Dog"
         },
         {
             id: 3,
             question: "Which show is set in the future and follows the adventures of a delivery boy?",
-            image: '../../public/images/futurama.jpg',
+            image: '../../images/futurama.jpg',
             options: ["Futurama", "The Jetsons", "Space Odyssey", "Tomorrowland"],
             answer: "Futurama"
         },
         {
             id: 4,
             question: "What is the name of the evil genius cat in 'Pinky and The Brain'?",
-            image: '../../public/images/pinky-brain.jpg',
+            image: '../../images/pinky-brain.jpg',
             options: ["Whiskers", "Snowball", "Fluffy", "Snowflake"],
             answer: "Snowball"
         },
         {
             id: 5,
             question: "What is the name of the pet dog owned by the Simpson family?",
-            image: '../../public/images/santas-little-helper.jpg',
+            image: '../../images/santas-little-helper.jpg',
             options: ["Rover", "Fido", "Santa's Little Helper", "Spot"],
             answer: "Santa's Little Helper"
         },
         {
             id: 6,
             question: "What is the name of the laboratory monkey in 'Dexter's Laboratory'?",
-            image: '../../public/images/monkey.jpg',
+            image: '../../images/monkey.jpg',
             options: ["Hairy Larry", "Monkey", "Bobo", "Fred"],
             answer: "Monkey"
         },
         {
             id: 7,
             question: "What is the catchphrase of Johnny Bravo?",
-            image: '../../public/images/johnny-bravo.jpg',
+            image: '../../images/johnny-bravo.jpg',
             options: ["'Hey there, pretty mama!'", "'Lookin' good!'", "'Whoa, mama!'", "'Good to see ya!'"],
             answer: "'Hey there, pretty mama!'"
         },
         {
             id: 8,
             question: "What is the name of Chuckie Finster's dad in 'Rugrats'?",
-            image: '../../public/images/chas.jpg',
+            image: '../../images/chas.jpg',
             options: ["Drew", "Stu", "Howard", "Chas"],
             answer: "Chas"
         },
         {
             id: 9,
             question: "What is the name of the character who is always trying to steal the Krabby Patty secret formula?",
-            image: '../../public/images/plankton.jpg',
+            image: '../../images/plankton.jpg',
             options: ["Larry the Lobster", "Plankton", "Pearl", "Mermaid Man"],
             answer: "Plankton"
         },
         {
             id: 10,
             question: "What is the name of the stone-age family's car in 'The Flintstones'?",
-            image: '../../public/images/flintmobile.jpg',
+            image: '../../images/flintmobile.jpg',
             options: ["Bedrock Buggy", "Rock-a-bye Car", "The Flintmobile", "Pebble Pusher"],
             answer: "The Flintmobile"
         },
         {
             id: 11,
             question: "Which show features a group of kids who are into extreme sports?",
-            image: '../../public/images/rocket-power.jpg',
+            image: '../../images/rocket-power.jpg',
             options: ["Extreme Teens", "Rocket Power", "Skater Boys", "Thrill Seekers"],
             answer: "Rocket Power"
         },
         {
             id: 12,
             question: "What is the name of the recurring mouse character who is Tom's nephew?",
-            image: '../../public/images/tuffy.jpg',
+            image: '../../images/tuffy.jpg',
             options: ["Tuffy", "Cheddar", "Nibbles", "Whiskers"],
             answer: "Tuffy"
         },
         {
             id: 13,
             question: "What is the name of CatDog's neighbor and best friend, a blue mouse?",
-            image: '../../public/images/winslow.jpg',
+            image: '../../images/winslow.jpg',
             options: ["Larry the Rat", "Winslow T. Oddfellow", "Chester Cheese", "Marty Mouse"],
             answer: "Winslow T. Oddfellow"
         },
         {
             id: 14,
             question: "What is the name of the robotic maid in 'The Jetsons'?",
-            image: '../../public/images/rosie.jpg',
+            image: '../../images/rosie.jpg',
             options: ["Rosie", "Roberta", "Robo-Maid", "Roxanne"],
             answer: "Rosie"
         },
         {
             id: 15,
             question: "What is the name of the main antagonist in 'Inspector Gadget', who is a criminal mastermind?",
-            image: '../../public/images/dr-claw.jpg',
+            image: '../../images/dr-claw.jpg',
             options: ["Baron von Badguy", "Professor Chaos", "The Mastermind", "Dr. Claw"],
             answer: "Dr. Claw"
         },
         {
             id: 16,
             question: "What is the name of the main character in 'The Fairly OddParents' who has two fairy godparents?",
-            image: '../../public/images/timmy.jpg',
+            image: '../../images/timmy.jpg',
             options: ["Tommy", "Timmy", "Jimmy", "Bobby"],
             answer: "Timmy"
         },
         {
             id: 17,
             question: "What is the name of the main character in 'The Angry Beavers' who is known for his neurotic behavior?",
-            image: '../../public/images/norbert.jpg',
+            image: '../../images/norbert.jpg',
             options: ["Daggett", "Barry", "Norbert", "Benny"],
             answer: "Norbert"
         },
         {
             id: 18,
             question: "Which character in 'Powerpuff Girls' is known for creating various inventions and gadgets?",
-            image: '../../public/images/powerpuff-girls.jpg',
+            image: '../../images/powerpuff-girls.jpg',
             options: ["Professor Utonium", "Mojo Jojo", "Mayor of Townsville", "Him"],
             answer: "Professor Utonium"
         },
         {
             id: 19,
             question: "In 'DuckTales,' what are the names of Huey, Dewey, and Louie's wealthy uncle?",
-            image: '../../public/images/ducktales.jpg',
+            image: '../../images/ducktales.jpg',
             options: ["Darkwing Duck", "Launchpad McQuack", "Scrooge McDuck", "Gyro Gearloose"],
             answer: "Scrooge McDuck"
         },
         {
             id: 20,
             question: "Which character in 'Aaahh!!! Real Monsters' is known for holding his eyes in his hands?",
-            image: '../../public/images/real-monsters.jpg',
+            image: '../../images/real-monsters.jpg',
             options: ["Ickis", "Oblina", "Krumm", "The Gromble"],
             answer: "Krumm"
         },
